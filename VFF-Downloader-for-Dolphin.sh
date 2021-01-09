ad9e32317053d9eb28bd7e3ae4fc75f9485e1967
SoulRepo <24778461+SoulRepo@users.noreply.github.com>
2021-01-08 22:39:12 -0500

    Created initial port file
    Basically just some variables, 10 minutes on a comment thing and no work on the program at all LOL

---
+++ VFF-Downloader-for-Dolphin.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+
+# variables and such
+last_build=2021/01/08
+at=10:02PM
+header=".VFF Downloader for Dolphin - KcrPL, Souliousery (Linux/Mac version) v$version (Built on $last_build at $at)"
+# ===================================================================================
+#            		.VFF FILE DOWNLOADER FOR DOLPHIN: SHELL EDITION
+
+				         		  version=1.0.4-bash
+
+#              		AUTHORS: KcrPL, Souliousery (ported to sh)
+#
+#  COPYRIGHT (c) 2021 KcrPL, Souliousery (shell only), RiiConnect24 and the Lead Devs
+#
+# ====================================================================================
+
